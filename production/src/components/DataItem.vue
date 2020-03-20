11473fb1e405d21ce530cd94addc75cc0b99cd42
Marianne Noordzij <62224722+hasitantadvisor@users.noreply.github.com>
2020-03-20 23:50:45 +0100

    css
    css

--- production/src/components/DataItem.vue
+++ production/src/components/DataItem.vue
@@ -1,10 +1,10 @@
 <template>
   <RouterLink :to="type == 'archive' ? `/archive/event/${event.id}` : `/event/${event.id}` " :class="{ playing: event.dateUnix_start <= time && event.dateUnix_end > time, ended: event.dateUnix_start < time && event.dateUnix_end < time }" class="data_item">
-    <p class="bold">{{ event.name }}</p>
+    <p class="bold">{{ event.name | truncate(400, '..') }}</p>
     {{type}}
 
     <p class="mono">{{ event.time_start }} CET</p>
-    <p v-if="event.initiator">by {{ event.initiator }}</p>
+    <p v-if="event.initiator">by {{ event.initiator | truncate(400, '..')}}</p>
     <div id="container" v-if="event.image">
       <img :src="event.image.url"/>
     </div>
@@ -15,9 +15,16 @@
 import Vue from 'vue'
 import moment from 'moment'
 
+
+
 export default Vue.extend({
   name: 'DataItem',
   props: [ 'event', 'time', 'type' ],
+  filters: {
+    truncate: function (text : string, stop : number, clamp : string ) {
+    return text.slice(0, stop) + (stop < text.length ? clamp || '...' : '');
+    }
+  }
 });
 </script>
 
@@ -73,15 +80,27 @@ p {
 
 
 .ended{
-  color:#A0A0A0;
+  color:black;
+  background-color:white;
+  /* border: 1px solid#A0A0A0; */
+
 }
 
-@media only screen and (max-width: 600px) {
+@media only screen and (max-width: 800px) {
   .data_item {
       width:50%;
       height:49.9vw;
       font-size:16px;
       hyphens: auto;
+      letter-spacing: -0.2px;
+  }
+
+  p {
+    line-height:1.1;
+    word-spacing: -1px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
   }
 }
 
